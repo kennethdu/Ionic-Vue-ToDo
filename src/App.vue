ce1d1bb0361d90250f082002ae12202e6075a55d
donuhead7 <kennethdu3@gmail.com>
2019-05-21 16:17:27 -0700

    barebones todo list

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,96 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <ToDoForm @add:task="addTask"/>
+    <ToDoTable :tasks="tasks"
+    @delete:task="deleteTask"
+    @edit:task="editTask"/>
   </div>
 </template>
 
+<script>
+import ToDoTable from "./components/ToDoTable";
+import ToDoForm from "./components/ToDoForm";
+
+export default {
+  name: "app",
+  components: {
+    ToDoTable,
+    ToDoForm
+  },
+  data() {
+    return {
+      tasks: []
+    };
+  },
+
+  mounted() {
+    this.getTasks();
+  },
+
+  methods: {
+    async getTasks() {
+      try {
+        const response = await fetch(
+          "https://my-json-server.typicode.com/kennethdu/to-do-vue-json/tasks"
+        );
+        const data = await response.json();
+        console.log(data);
+        this.tasks = data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async addTask(task) {
+      try {
+        const response = await fetch(
+          "https://my-json-server.typicode.com/kennethdu/to-do-vue-json/tasks",
+          {
+            method: "POST",
+            body: JSON.stringify(task),
+            headers: {
+              "Content-type": "application/json; charset=UTF-8"
+            }
+          }
+        );
+        const data = await response.json();
+        this.tasks = [...this.tasks, data];
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async editTask(id, updatedTask){
+      try {
+        const response = await fetch (`https://my-json-server.typicode.com/kennethdu/to-do-vue-json/tasks/${id}`, {
+          method: 'PUT',
+          body: JSON.stringify(updatedTask),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8"
+          }
+        })
+        const data = await response.json()
+        this.tasks = this.tasks.map(tasks => tasks.id === id ? data : tasks)
+      } catch (error){
+        console.error(error)
+      }
+    },
+    async deleteTask(id){
+      try{
+        await fetch(`https://my-json-server.typicode.com/kennethdu/to-do-vue-json/tasks/${id}`, {
+          method: 'DELETE'
+        })
+        this.tasks = this.tasks.filter(tasks => tasks.id !== id)
+      } catch (error){
+        console.error(error)
+      }
+    }
+  }
+};
+</script>
+
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
